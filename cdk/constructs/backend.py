b756f9a608fb4627110bdecf45c00b98db179f4e
Jesse Goeglein <jesse.goeglein@driverai.com>
2024-06-14 16:34:45 -0600

    Increase CPU and memory, enable circuit breaker

--- cdk/constructs/backend.py
+++ cdk/constructs/backend.py
@@ -78,7 +78,10 @@ class Backend(Construct):
             domain_name="api." + hosted_zone.zone_name,\
             task_image_options=task_options,\
             task_subnets=aws_ec2.SubnetSelection(subnet_type=aws_ec2.SubnetType.PRIVATE_WITH_EGRESS),\
-            health_check_grace_period=Duration.minutes(5))
+            health_check_grace_period=Duration.minutes(5),\
+            circuit_breaker=aws_ecs.DeploymentCircuitBreaker(enable=True, rollback=True),\
+            cpu=1,\
+            memory_limit_mib=1024)
         service.target_group.configure_health_check(path="/api/v1/healthcheck/", port="8888")
 
         waf_visibility_config = aws_wafv2.CfnWebACL.VisibilityConfigProperty(cloud_watch_metrics_enabled=True, metric_name="MetricForWebACLCDK", sampled_requests_enabled=True)

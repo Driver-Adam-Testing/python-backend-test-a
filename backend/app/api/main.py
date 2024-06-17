4b6d1920008d99dfa15f48c7a2d159fb76db01b6
Jesse Goeglein <jesse.goeglein@driverai.com>
2024-06-17 15:46:48 -0600

    GraphQL first

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -6,8 +6,8 @@ from app.api.routes.v1 import workspace, healthcheck
 
 api_router = APIRouter()
 
+api_router.include_router(graphql_router, prefix="/graphql", tags=["legacy-graphql"])
 api_router.include_router(workspace.router, prefix="/workspace", tags=["workspace"])
 api_router.include_router(healthcheck.router, prefix="/healthcheck", tags=["healthcheck"])
-api_router.include_router(graphql_router, prefix="/graphql", tags=["legacy-graphql"])
 if(settings.ENVIRONMENT != "production"):
     api_router.include_router(sandbox_router, prefix="/sandbox", tags=["legacy-sandbox"])

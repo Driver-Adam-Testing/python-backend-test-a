0ac0138e10336eb313a510935a061c768486d611
Jesse Goeglein <jesse.goeglein@driverai.com>
2024-06-18 12:21:26 -0600

    DRI-869 disable parts of prestart to test

--- backend/prestart.sh
+++ backend/prestart.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/env bash
 
 # Let the DB start
-python /app/app/backend_pre_start.py
+# python /app/app/backend_pre_start.py
 
 # # Run migrations
 (cd /driver_db/database && alembic upgrade head)
 
 # Create initial data in DB
-python /app/app/initial_data.py
+# python /app/app/initial_data.py

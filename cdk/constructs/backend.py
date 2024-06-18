42698722d145a172d6a9c0d27eef8ab012533b8f
Jesse Goeglein <jesse.goeglein@driverai.com>
2024-06-18 13:50:17 -0600

    Use start-reload, adjust resource sizes

--- cdk/constructs/backend.py
+++ cdk/constructs/backend.py
@@ -91,8 +91,10 @@ class Backend(Construct):
             task_subnets=aws_ec2.SubnetSelection(subnet_type=aws_ec2.SubnetType.PRIVATE_WITH_EGRESS),\
             health_check_grace_period=Duration.minutes(5),\
             circuit_breaker=aws_ecs.DeploymentCircuitBreaker(enable=True, rollback=True),\
-            cpu=1024,\
-            memory_limit_mib=2048)
+            min_healthy_percent=100,\
+            max_healthy_percent=250,\
+            cpu=512,\
+            memory_limit_mib=1024)
         service.target_group.configure_health_check(path="/api/v1/healthcheck/", port="8000")
         service.task_definition.task_role.attach_inline_policy(aws_iam.Policy(self, "CustomerSecretsRW", document=aws_iam.PolicyDocument(statements=[\
             aws_iam.PolicyStatement(\

12823709af802f9e0fd8df4c9be80213cc9e96d9
Shane  Ghiotto <sghiotto1@gmail.com>
2024-06-20 12:53:29 -0700

    Reorganized database calls into single transaction

--- content_services/onboarding/src/codebase_onboarding.py
+++ content_services/onboarding/src/codebase_onboarding.py
@@ -31,26 +31,6 @@ image = (
 def create_base_storage_url(org_id: str):
     return f"https://{org_id}.s3.amazonaws.com"
 
-@dataclass
-class CodebaseInput:
-    codebase_name: str
-    creator_id: str
-    description: str | None
-    resource_root: str
-    storage_url: str
-    workspace_id: UUID 
-    status: Enum_Codebase_Status
-
-
-@dataclass
-class SourceContentInput:
-    codebase_id: UUID
-    relative_path: str
-    source_content_type_id: UUID 
-    workspace_id: UUID
-    analysis_metadata: dict
-
-
 @cache
 def get_source_content_type_uuid(content_type_name: str) -> UUID:
     sct_uuid = None
@@ -71,37 +51,6 @@ def create_bucket_if_dne(bucket_name: str) -> None:
         # Bucket does not exist
         s3_resource.create_bucket(Bucket=bucket_name)
         print(f'Created bucket: {bucket_name}')
-
-
-def create_codebase(cb_input: CodebaseInput) -> UUID:
-    codebase = Codebase(
-        **asdict(cb_input)
-    )
-    cb_id = None
-    print(os.environ['DATABASE_URL'])
-    print(os.path.exists('/root/data/dev_db_public_key.cert'))
-    with Session(engine) as session:
-        session.add(codebase)
-        session.commit()
-        session.refresh(codebase)
-
-        if codebase.id:
-            cb_id = codebase.id
-            # Update the storage URL
-            final_storage_url = urljoin(codebase.storage_url, str(codebase.id))
-            print(final_storage_url)
-            codebase.storage_url = final_storage_url
-            session.commit()
-
-    return cb_id 
-
-def create_source_content(sc_input: SourceContentInput) -> None:
-    source_content = SourceContent(
-        **asdict(sc_input)
-    )
-    with Session(engine) as session:
-        session.add(source_content)
-        session.commit()
 
 def download_file_from_s3(
     bucket_name: str, 
@@ -173,7 +122,6 @@ def upload_file_to_s3(
     s3_resource = resource("s3", endpoint_url=os.environ.get("AWS_S3_ENDPOINT_URL"))
     #TODO: if S3 is reorged, bucket is consistent?
     s3_bucket = s3_resource.Bucket(bucket_name) 
-    print("Bucket name: ", bucket_name)
     if local_path.exists():
         s3_destination_path = destination_root / local_path
         s3_bucket.upload_file(local_path, str(s3_destination_path))
@@ -338,12 +286,8 @@ def analyze_binary_file(filepath: Path) -> dict:
         'is_hex' : False
     }
 
-def run_file_onboarding(
+def run_file_stats_and_reencode(
     local_path: Path, 
-    bucket_name: str, 
-    dest_root: str,
-    codebase_id: UUID,
-    workspace_id: UUID
 ) -> dict:
     # Evaluate file-processability before reencoding 
     # due to file encoding nastiness w/ binary files
@@ -366,20 +310,6 @@ def run_file_onboarding(
         file_stats['is_analyzable'] = False
     file_stats['is_blacklisted'] = is_blacklisted
 
-    if not is_blacklisted:
-        uploaded_dest_path = upload_file_to_s3(bucket_name, dest_root, local_path)
-
-        file_sc_type = get_source_content_type_uuid("codebase-file")
-        file_sc_input = SourceContentInput(
-            codebase_id=codebase_id,
-            relative_path=str(local_path),
-            source_content_type_id=file_sc_type,
-            workspace_id=workspace_id,
-            analysis_metadata=file_stats
-        )
-        create_source_content(file_sc_input)
-
-        print(f"Onboarded File: {local_path} to {uploaded_dest_path} on S3. Processable: {file_stats['is_analyzable']}. Stats: {file_stats}")
     return file_stats
 
 @app.function(
@@ -421,71 +351,102 @@ def run_codebase_onboarding(
     if extracted_path.exists():
         upload_bucket = org_id
         create_bucket_if_dne(upload_bucket)
-
-        base_url = create_base_storage_url(org_id)
-        cb_input = CodebaseInput(
-            codebase_name=str(extracted_path),
-            creator_id=creator_id,
-            description='',
-            resource_root=f"{str(extracted_path)}/",
-            storage_url=base_url,
-            workspace_id=workspace_id,
-            status=Enum_Codebase_Status.processing
-        )
-        codebase_id = create_codebase(cb_input)
-        if not codebase_id:
-            print("Codebase creation failed for: ", extracted_path)
-
-        cb_sc_uuid = get_source_content_type_uuid("codebase")
-        if not cb_sc_uuid:
-            print("couldn't find source content type named codebase")
-
-        cb_sc_input = SourceContentInput(
-            codebase_id=codebase_id,
-            relative_path=str(extracted_path),
-            source_content_type_id=cb_sc_uuid,
-            workspace_id=workspace_id,
-            analysis_metadata=dict()
-        )
-        create_source_content(cb_sc_input)
-        
-        s3_dest_root = Path(str(codebase_id)) / "source"
+        codebase_id = uuid4()
 
         for root, dirs, files in os.walk(extracted_path):
             all_directories.append(root)
             for filename in files:
                 local_path = Path(root) / filename
 
-                file_stats = run_file_onboarding(
+                file_stats = run_file_stats_and_reencode(
                     local_path, 
-                    upload_bucket, 
-                    s3_dest_root,
-                    codebase_id,
-                    workspace_id
                 )
-                codebase_stats[str(local_path)] = file_stats
-    
-    dir_sc_uuid = get_source_content_type_uuid("codebase-directory")
-    if not dir_sc_uuid:
-        print("couldn't find source content type named codebase-directory")
+                codebase_stats[local_path] = file_stats
 
-    for directory in all_directories:
-        if not is_on_blacklist(Path(directory)):
-            # TODO: analysis metadata for directories? 
-            # TODO: this is fragile - consider using DAG logic here 
-            dir_sc_input = SourceContentInput(
-                codebase_id=codebase_id,
-                relative_path=directory,
-                source_content_type_id=dir_sc_uuid,
-                workspace_id=workspace_id,
-                analysis_metadata=dict()
-            )
-            create_source_content(dir_sc_input)
+        s3_dest_root = Path(str(codebase_id)) / "source"
+        # TODO: put this in a threadpoolexecutor
+        for file_path in codebase_stats.keys():
+            if not codebase_stats[file_path]['is_blacklisted']:
+                uploaded_dest_path = upload_file_to_s3(upload_bucket, s3_dest_root, file_path)
 
+        with Session(engine) as session:
+            with session.begin():
+                base_url = create_base_storage_url(org_id)
+                final_storage_url = urljoin(base_url, str(codebase_id))
+                codebase = Codebase(
+                    id=codebase_id,
+                    codebase_name=str(extracted_path),
+                    creator_id=creator_id,
+                    description='',
+                    resource_root=f"{str(extracted_path)}/",
+                    storage_url=final_storage_url,
+                    workspace_id=workspace_id,
+                    status=Enum_Codebase_Status.processing
+                )
+                session.add(codebase)
+                # Flush here to confirm that Source Contents created after this will know that the 
+                # codebase exists
+                session.flush()
+                print(f"Created but not commited codebase: {str(extracted_path)}, with ID: {codebase_id}")
+
+                cb_sc_uuid = get_source_content_type_uuid("codebase")
+                cb_sc = SourceContent(
+                    codebase_id=codebase_id,
+                    relative_path=str(extracted_path),
+                    source_content_type_id=cb_sc_uuid,
+                    workspace_id=workspace_id,
+                    analysis_metadata=dict()
+                )
+                session.add(cb_sc)
+
+                # Add directories source contents
+                dir_sc_uuid = get_source_content_type_uuid("codebase-directory")
+                for directory in all_directories:
+                    if not is_on_blacklist(Path(directory)):
+                        # TODO: analysis metadata for directories? 
+                        # TODO: this is fragile - consider using DAG logic here 
+                        dir_sc = SourceContent(
+                            codebase_id=codebase_id,
+                            relative_path=directory,
+                            source_content_type_id=dir_sc_uuid,
+                            workspace_id=workspace_id,
+                            analysis_metadata=dict()
+                        )
+                        session.add(dir_sc)
+                        print(f"Created but not commited source content for: {directory}.")
+
+                # Add file source contents
+                for file_path in codebase_stats.keys():
+                    if not codebase_stats[file_path]['is_blacklisted']:
+                        file_sc_type = get_source_content_type_uuid("codebase-file")
+                        file_sc = SourceContent(
+                            codebase_id=codebase_id,
+                            relative_path=str(file_path),
+                            source_content_type_id=file_sc_type,
+                            workspace_id=workspace_id,
+                            analysis_metadata=codebase_stats[file_path]
+                        )
+                        session.add(file_sc)
+
+                        print(f"Created but not commited source content for: {file_path}. Processable: {codebase_stats[file_path]['is_analyzable']}. Stats: {codebase_stats[file_path]}")
+
+    print("Codebase onboarding complete for codebase id: ", codebase_id)
     return codebase_id
 
 @app.function(
-    image=modal.Image.debian_slim(python_version="3.12"),
+    image=image,
+    mounts=[
+        modal.Mount.from_local_python_packages("database"),
+        modal.Mount.from_local_dir(
+            local_path="../../driver_db/certs/",
+            remote_path="/root/data/",
+        )
+    ],
+    secrets=[
+        modal.Secret.from_name("aws-inspector-s3"),
+        modal.Secret.from_name("db")
+    ],
+    proxy=modal.Proxy.from_name("pg-proxy"),
     timeout=24*60*60
 )
 def onboard_and_inspect(dropzone_bucket_name:str, archive_name: str, org_id: str, creator_id: str, workspace_id: UUID):
@@ -493,6 +454,7 @@ def onboard_and_inspect(dropzone_bucket_name:str, archive_name: str, org_id: str
 
     codebase_id = run_codebase_onboarding.remote(dropzone_bucket_name, archive_name, org_id, creator_id, workspace_id)
     print("onboarding complete for codebase: ", codebase_id)
+    print("Inspecting...")
 
     run_id = uuid4()
     inspect_db.remote(codebase_id, run_id)
@@ -503,7 +465,7 @@ def main():
     # s3_bucket_name = "modal-dev-inspector-1234442"
     s3_bucket_name = "development-codebase-dropzone"
     s3_prefix = "codebases"
-    archive_name = "python-dotenv.zip"
+    archive_name = "mtb-example-psoc6-crypto-aes.zip"
     # create_bucket_if_dne(s3_bucket_name)
 
     # # Upload to bucket if doesn't exist

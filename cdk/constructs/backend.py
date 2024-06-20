ee50ce6e1555065821c2acc14bf556106e671a06
Jesse Goeglein <jesse.goeglein@driverai.com>
2024-06-20 10:49:49 -0600

    Block non-VPN IP addresses

--- cdk/constructs/backend.py
+++ cdk/constructs/backend.py
@@ -106,7 +106,8 @@ class Backend(Construct):
         waf_visibility_config_ips = aws_wafv2.CfnWebACL.VisibilityConfigProperty(cloud_watch_metrics_enabled=True, metric_name="MetricForWebACLCDK-IPs", sampled_requests_enabled=True)
         whitelist_ip_set = aws_wafv2.CfnIPSet(self, "WhitelistIPs", ip_address_version="IPV4", scope="REGIONAL", addresses=params.allowed_ips)
         ipset_rule_statement = aws_wafv2.CfnWebACL.StatementProperty(ip_set_reference_statement=aws_wafv2.CfnWebACL.IPSetReferenceStatementProperty(arn=whitelist_ip_set.attr_arn))
-        ipset_rule = aws_wafv2.CfnWebACL.RuleProperty(name="AllowedIPs", priority=0, statement=ipset_rule_statement, visibility_config=waf_visibility_config_ips, action=aws_wafv2.CfnWebACL.RuleActionProperty(allow={}))
+        n = aws_wafv2.CfnWebACL.NotStatementProperty(statement=ipset_rule_statement)
+        ipset_rule = aws_wafv2.CfnWebACL.RuleProperty(name="AllowedIPs", priority=0, statement=aws_wafv2.CfnWebACL.StatementProperty(not_statement=n), visibility_config=waf_visibility_config_ips, action=aws_wafv2.CfnWebACL.RuleActionProperty(block={}))
 
         waf_visibility_config = aws_wafv2.CfnWebACL.VisibilityConfigProperty(cloud_watch_metrics_enabled=True, metric_name="MetricForWebACLCDK", sampled_requests_enabled=True)
         waf_visibility_config_crs = aws_wafv2.CfnWebACL.VisibilityConfigProperty(cloud_watch_metrics_enabled=True, metric_name="MetricForWebACLCDK-CRS", sampled_requests_enabled=True)

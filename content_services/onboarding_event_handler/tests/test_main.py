e0c3733ce94f5b45488b30bdc4b7aba48bd06069
Eric Miller <146128069+eric-driverai@users.noreply.github.com>
2024-06-19 21:27:20 -0500

    Remove unused test setup and AWS credentials mock
    The changes include cleaning up some unused code in the onboarding_event_handler tests, specifically the AWS credentials mock. This included a reduction in the usage of aws_credentials and the removal of some commented lines. The modifications promote clean and uncluttered code for the tests.

--- content_services/onboarding_event_handler/tests/test_main.py
+++ content_services/onboarding_event_handler/tests/test_main.py
@@ -9,22 +9,6 @@ from httpx import Response
 from src.main import lambda_handler
 from src.config import settings
 
-
-# from moto.s3
-# from moto.sns import mock_sns
-# Add the src directory to the system path
-
-
-@pytest.fixture
-def aws_credentials():
-    """Mocked AWS Credentials for moto."""
-    import os
-    os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
-    os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
-    os.environ['AWS_SECURITY_TOKEN'] = 'testing'
-    os.environ['AWS_SESSION_TOKEN'] = 'testing'
-
-
 @pytest.fixture
 def sns_event():
     """Generates a mock SNS event"""

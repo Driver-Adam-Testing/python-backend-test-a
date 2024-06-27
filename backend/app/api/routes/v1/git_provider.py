572ee66a4a1049f8bda071f78f8962e037267bfa
Eric Miller <146128069+eric-driverai@users.noreply.github.com>
2024-06-27 16:55:43 -0500

    Remove GitHub auth and install routings
    The GitHub auth and install routings have been deleted from the code. The functions provider_auth() and install_url() from the git_provider routes have also been removed. This commit makes the code simpler and ensures unnecessary functionalities don't exist.

--- backend/app/api/routes/v1/git_provider.py
+++ backend/app/api/routes/v1/git_provider.py
@@ -24,25 +24,6 @@ class OkResponse(BaseModel):
     status: str = "OK"
 
 
-@router.get("/{provider}/auth")
-async def provider_auth(request: Request, response: Response, provider: str):
-    client_id = settings.GH_CLIENT_ID
-    redirect_uri = settings.GH_REDIRECT_URI
-    github_auth_url = f"https://github.com/login/oauth/authorize?client_id={client_id}&scope=repo&redirect_uri={redirect_uri}"
-    print(github_auth_url)
-    response.status_code = HTTP_302_FOUND
-    response.headers["Location"] = github_auth_url
-
-
-@router.get("/{provider}/install", response_model=OkResponse)
-def install_url(org_id: str, user_id: str):
-    redirect_uri = settings.GH_REDIRECT_URI
-    state_dict = json.dumps({"org_id": org_id, "user_id": user_id})
-    state = base64.b64encode(state_dict.encode('utf-8')).decode('utf-8')
-    install_link = f"https://github.com/apps/driverai-gh-demo/installations/new?state={state}&redirect_uri={redirect_uri}"
-    return OkResponse(status=install_link)
-
-
 @router.get("/{provider}/callback", response_model=OkResponse)
 async def git_provider_callback(provider: str, code: str, state: str, installation_id: str, request: Request,
                                 response: Response):
@@ -55,7 +36,7 @@ async def git_provider_callback(provider: str, code: str, state: str, installati
     state_bytes = base64.b64decode(state)
     state_str = state_bytes.decode('utf-8')
     state_dict = json.loads(state_str)
-
+    #TODO: validate state_dict
     org_id, user_id = state_dict["org_id"], state_dict["user_id"]
     secret_key = format_secret_key(org_id, user_id, provider)
     token_data = await exchange_code_for_token(code)

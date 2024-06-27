b9698db5113922617204fec9fc3896ee685d987e
Jesse Goeglein <jesse.goeglein@driverai.com>
2024-06-27 11:49:24 -0600

    Increase dev task resources

--- cdk/constructs/backend.py
+++ cdk/constructs/backend.py
@@ -103,8 +103,8 @@ class Backend(Construct):
             circuit_breaker=aws_ecs.DeploymentCircuitBreaker(enable=True, rollback=True),
             min_healthy_percent=100,
             max_healthy_percent=250,
-            cpu=512,
-            memory_limit_mib=1024)
+            cpu=1024,
+            memory_limit_mib=2048)
         service.target_group.configure_health_check(path="/api/v1/healthcheck/", port="8000")
         service.task_definition.task_role.attach_inline_policy(aws_iam.Policy(self, "CustomerSecretsRW", document=aws_iam.PolicyDocument(statements=[
             aws_iam.PolicyStatement(

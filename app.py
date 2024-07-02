acd13e311f9d9e5a92006c8c0c405a0af6fd681c
Jesse Goeglein <jesse.goeglein@driverai.com>
2024-07-02 09:24:00 -0600

    Add GH Action for main branch

--- app.py
+++ app.py
@@ -4,46 +4,27 @@ import os
 import aws_cdk as cdk
 from cdk.development_stack import DevelopmentStack
 from cdk.ops_stack import OpsStack
+from cdk.production_stack import ProductionStack
 
 app = cdk.App()
 
 deployment_environment = os.getenv('DEPLOYMENT_ENVIRONMENT')
 
-if deployment_environment == "development":
+if deployment_environment == "local":
     DevelopmentStack(app, "DriverApiStack",
-        # If you don't specify 'env', this stack will be environment-agnostic.
-        # Account/Region-dependent features and context lookups will not work,
-        # but a single synthesized template can be deployed anywhere.
-
-        # Uncomment the next line to specialize this stack for the AWS Account
-        # and Region that are implied by the current CLI configuration.
-
         env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-        # Uncomment the next line if you know exactly what Account and Region you
-        # want to deploy the stack to. */
-
-        #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-        # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-        )
+    )
+elif deployment_environment == "development":
+    DevelopmentStack(app, "DriverApiStack",
+        env=cdk.Environment(account="550082761109", region="us-east-1"),
+    )
 elif os.getenv('DEPLOYMENT_ENVIRONMENT') == "ops":
     OpsStack(app, "DriverApiStack",
-        # If you don't specify 'env', this stack will be environment-agnostic.
-        # Account/Region-dependent features and context lookups will not work,
-        # but a single synthesized template can be deployed anywhere.
-
-        # Uncomment the next line to specialize this stack for the AWS Account
-        # and Region that are implied by the current CLI configuration.
-
-        env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-        # Uncomment the next line if you know exactly what Account and Region you
-        # want to deploy the stack to. */
-
-        #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-        # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-        )
+        env=cdk.Environment(account="058264523856", region="us-east-1"),
+    )
+elif os.getenv('DEPLOYMENT_ENVIRONMENT') == "production":
+    ProductionStack(app, "DriverApiStack",
+        env=cdk.Environment(account="896724907114", region="us-east-1"),
+    )
 
 app.synth()
--- cdk/production_stack.py
+++ cdk/production_stack.py
@@ -4,8 +4,18 @@ from aws_cdk import (
 )
 from constructs import Construct
 
+from cdk.constructs.backend import Backend, BackendParams
+from cdk.constructs.onboarding_lambda import OnboardingLambda, OnboardingLambdaParams
+
 class ProductionStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+        
+        backend = Backend(self, "ApiBackend", BackendParams(
+            environment="production",
+            cors_origins="https://app.driverai.com",
+            allowed_ips=[] # Currently disabled
+        ))
+        onboarding_lambda = OnboardingLambda(self, "OnboardingLambda", OnboardingLambdaParams(environment="production", api_url="https://api.us1.driverai.com/api/v1", auth0_url="https://auth.driverai.com"))
 

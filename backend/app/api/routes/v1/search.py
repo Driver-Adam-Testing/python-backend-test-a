1aa0e113366efb2a8cfe37cd0f69db4942bdf51c
Neil Zumwalde <neilzumwalde@gmail.com>
2024-07-10 16:37:48 -0700

    forgot the search file

---
+++ backend/app/api/routes/v1/search.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter
+
+from app.api.auth import CurrentUser
+from app.api.session import CurrentSession
+from app.utils.search import SearchInput, SearchResults, search_content_metadata
+
+router = APIRouter()
+
+
+@router.post(
+    "/",
+    summary="Search for content",
+    response_description="Return HTTP Status Code 200 (OK)",
+)
+def search(
+    session: CurrentSession, user: CurrentUser, input: SearchInput
+) -> SearchResults:
+    return search_content_metadata(
+        session=session, organization_id=user.organization_id, input=input
+    )

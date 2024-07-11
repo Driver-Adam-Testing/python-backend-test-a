2d6d26c5e785a315a696cf6eed5a85cf56c87d58
Eric Miller <146128069+eric-driverai@users.noreply.github.com>
2024-07-11 18:46:26 -0500

    Update document paths and source content ID in run.py
    The commit updates the 'codebase_id', 'relative_path', and 'source_content_id' in run.py. This change is necessary to correctly locate and process a new set of documents from a different codebase and source content.

--- content_services/pdf_preprocessing/src/run.py
+++ content_services/pdf_preprocessing/src/run.py
@@ -10,8 +10,8 @@ async def main():
     org_id = 'org_s76pU1v8LAYhTOWB'
     org_id_hash = hashlib.sha256(org_id.encode()).hexdigest()[:63]
 
-    codebase_id = '95b897f7-3910-497a-8ca3-59882d1c0e3f'
-    relative_path = 'documents/cn0556.pdf'
+    codebase_id = '8aed1938-adca-4097-801f-ee7121fc9067'
+    relative_path = 'documents/AD4114+%281%29.pdf'
 
     object_key = f"{codebase_id}/{relative_path}"
     presigned_url = generate_get_presigned_url(key=object_key, bucket=org_id_hash)
@@ -19,7 +19,7 @@ async def main():
     await preprocess(PdfInput(
         presigned_url=presigned_url,
         pdf_name=None,
-        source_content_id='4f6e2a72-3f69-47ff-b967-b43733cb158e'
+        source_content_id='e81caf63-95bd-4f34-b84f-0f750df1da40'
     ))
 
 

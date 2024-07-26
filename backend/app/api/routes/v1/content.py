dda6fbec04ffb4706c269d333b52ac4e0c21ce40
Jesse Goeglein <jesse.goeglein@driverai.com>
2024-07-26 11:46:07 -0600

    wip: Initial list content and types endpoints

--- backend/app/api/routes/v1/content.py
+++ backend/app/api/routes/v1/content.py
@@ -1,8 +1,16 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Query
 
 from app.api.auth import CurrentUser
 from app.api.session import CurrentSession
-from app.utils.content import ListContentResults, list_content
+from app.utils.content import (
+    ListContentInput,
+    ListContentResults,
+    ListContentTypesResults,
+    list_content,
+    list_content_types,
+)
 
 router = APIRouter()
 
@@ -10,7 +18,6 @@ router = APIRouter()
 @router.get(
     "/",
     summary="List content matching the provided filter criteria",
-    response_description="Return HTTP Status Code 200 (OK)",
 )
 def list(
     session: CurrentSession,
@@ -18,9 +25,31 @@ def list(
     limit: int | None = 20,
     text: str | None = None,
     offset: int | None = 0,
-    content_type: list[str] | None = None,
-    # sort: "ASC" | "DESC" | None = None TODO - grouped sorting on the server gets complicated too
-    # date: TODO - format? single date? date ranges? > date?
-    labels: list[str] | None = None,
+    content_type_id: Annotated[list[str], Query()] | None = None,
+    sort_by: str | None = None,
+    sort_direction: str | None = "ASC",
+    status: str | None = None,
+    tags: Annotated[list[str], Query()] | None = None,
 ) -> ListContentResults:
-    return list_content(session, user, input)
+    return list_content(
+        session,
+        user,
+        ListContentInput(
+            limit=limit,
+            offset=offset,
+            text=text,
+            content_type_id=content_type_id,
+            sort_by=sort_by,
+            sort_direction=sort_direction,
+            status=status,
+            tags=tags,
+        ),
+    )
+
+
+@router.get(
+    "/types",
+    summary="List content types",
+)
+def list_types(session: CurrentSession, user: CurrentUser) -> ListContentTypesResults:
+    return list_content_types(session)
--- backend/app/utils/content.py
+++ backend/app/utils/content.py
@@ -1,100 +1,75 @@
-from database.models_v1 import DerivedContent
+from database.models_v1 import DerivedContent, DerivedContentType, Workspace
 from pydantic import BaseModel
-from sqlmodel import Session
+from sqlmodel import Session, asc, desc, select
 
 from app.api.auth import CurrentUser
+from app.core.logger import logger
 
 
 class ListContentInput(BaseModel):
     text: str | None
     limit: int | None = 20
     offset: int | None = 0
-    sort: str | None = None
-    # sortDirection: "ASC" | "DESC" | None = None
-    # date: TODO - format? single date? date ranges? > date?
-    content_type: list[
-        str
-    ] | None = None  # TODO - how do we get these strings to the UI? Do we need a list of derived content types?
-    labels: list[str] | None = None
+    sort_by: str | None = None
+    sort_direction: str | None = "DESC"
+    status: str | None = None
+    content_type_id: list[str] | None = None
+    tags: list[str] | None = None
 
 
 class ListContentResults(BaseModel):
-    results: list[
-        DerivedContent
-    ]  # TODO - separate models from DB? This is where Andrew is migrating content to
+    results: list[DerivedContent]
     offset: int
     limit: int
 
 
+class ListContentTypesResults(BaseModel):
+    results: list[DerivedContentType]
+
+
 def list_content(session: Session, user: CurrentUser, input: ListContentInput):
-    return ListContentResults(result=[], offset=1, limit=2)
-    # embedded_query = TextEmbedder().batch_embed_text([input.query])[0]
+    statement = (
+        select(DerivedContent)
+        .join(Workspace)
+        .where(user.organization_id == Workspace.organization_id)
+        # .join(Codebase)
+        # .filter(
+        #     or_(
+        #         [
+        #             user.organization_id == Codebase.workspace.organization_id,
+        #             user.organization_id == Workspace.organization_id,
+        #         ]
+        #     )
+        # )
+        .offset(input.offset)
+        .limit(input.limit)
+    )
+    if input.sort_by:
+        if input.sort_direction == "ASC":
+            statement = statement.order_by(asc(input.sort_by))
+        elif input.sort_direction == "DESC":
+            statement = statement.order_by(desc(input.sort_by))
+        else:
+            logger.error("Invalid sort direction provided.")
+            raise RuntimeError(
+                "Invalid sort direction provided. Options are ASC or DESC"
+            )
 
-    # statement = select(
-    #     Chunk,
-    #     ContentMetadata,
-    #     Chunk.text_embedding_3_small.l2_distance(embedded_query).label("score"),
-    # ).where(ContentMetadata.id == Chunk.content_metadata_id)
+    if input.text:
+        statement = statement.where(DerivedContent.relative_path.contains(input.text))
 
-    # if input.workspace_id:
-    #     statement = statement.where(ContentMetadata.workspace_id == input.workspace_id)
+    if input.status:
+        statement = statement.where(DerivedContent.status == input.status)
 
-    # if input.codebase_id:
-    #     statement = statement.where(ContentMetadata.codebase_id == input.codebase_id)
+    if input.content_type_id and len(input.content_type_id) > 0:
+        statement = statement.where(
+            DerivedContent.content_type_id.in_(input.content_type_id)
+        )
 
-    # if input.content_type:
-    #     if isinstance(input.content_type, str):
-    #         statement = statement.where(
-    #             ContentMetadata.content_type == input.content_type
-    #         )
-    #     elif isinstance(input.content_type, list):
-    #         statement = statement.where(
-    #             ContentMetadata.content_type.in_(input.content_type)
-    #         )
+    results = session.exec(statement).all()
+    return ListContentResults(results=results, offset=input.offset, limit=input.limit)
 
-    # if input.relative_path:
-    #     statement = statement.where(
-    #         or_(
-    #             *[
-    #                 ContentMetadata.relative_path.like(f"{file_path}%")
-    #                 for file_path in input.relative_path
-    #             ]
-    #         )
-    #     )
 
-    # statement = statement.order_by(asc("score"))
-
-    # if input.result_limit:
-    #     statement = statement.limit(input.result_limit)
-
-    # results = session.exec(statement).all()
-    # search_results = []
-
-    # accumulated_tokens = 0
-    # for c, cm, score in results:
-    #     c: Chunk = c
-    #     cm: ContentMetadata = cm
-    #     if input.token_limit is not None:
-    #         if accumulated_tokens > input.token_limit:
-    #             break
-    #     token_count = (
-    #         c.token_count
-    #         if c.token_count is not None
-    #         else int(len(c.text.split()) / 2.5)
-    #     )
-    #     accumulated_tokens += token_count
-    #     search_results.append(
-    #         SearchResult(
-    #             content=c.text,
-    #             score=score,
-    #             metadata={
-    #                 "content_type": cm.content_type,
-    #                 "relative_path": cm.relative_path,
-    #                 "workspace_id": cm.workspace_id,
-    #                 "codebase_id": cm.codebase_id,
-    #                 "chunk_number": c.chunk_number,
-    #                 "line_number": c.line_number,
-    #             },
-    #         )
-    #     )
-    # return SearchResults(results=search_results)
+def list_content_types(session: Session):
+    results = session.exec(select(DerivedContentType)).all()
+    return ListContentTypesResults(results=results)

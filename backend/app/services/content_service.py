1be0da93fa9de87c0b9ba241f27df2388256b80c
Jesse Goeglein <jesse.goeglein@driverai.com>
2024-08-13 11:03:05 -0500

    Refer to top-level table name

--- backend/app/services/content_service.py
+++ backend/app/services/content_service.py
@@ -1,33 +1,26 @@
 import json
 from datetime import datetime
-from typing import Optional
-
-from fastapi import HTTPException
-
-from sqlmodel import Session, asc, desc, or_, select, func, text
-
-from app.schemas.content_schema import (
-    CreateContentResponse,
-    CreateTemplateResponse,
-    ListContentInput,
-    ListContentResults,
-    ListContentResult,
-    ListContentTypesInput,
-    ListContentTypesResults,
-)
-
-from app.api.session import CurrentSession
-
-from app.repositories.base_repository import BaseRepository
-from sqlalchemy.exc import NoResultFound
 
 from database.models_v1 import (
     DerivedContent,
-    Workspace,
     DerivedContentType,
     Enum_Derived_Content_Status,
+    Tag,
     TagContent,
-    Tag
+    Workspace,
+)
+from fastapi import HTTPException
+from sqlalchemy.exc import NoResultFound
+from sqlmodel import Session, asc, desc, func, or_, select, text
+
+from app.api.session import CurrentSession
+from app.repositories.base_repository import BaseRepository
+from app.schemas.content_schema import (
+    ListContentInput,
+    ListContentResult,
+    ListContentResults,
+    ListContentTypesInput,
+    ListContentTypesResults,
 )
 
 
@@ -37,8 +30,7 @@ class DerivedContentTypeRepository(BaseRepository[DerivedContentType]):
 
     def get_by_type_name(self, type_name: str) -> DerivedContentType:
         return self.session.exec(
-            select(DerivedContentType)
-            .where(DerivedContentType.type_name == type_name)
+            select(DerivedContentType).where(DerivedContentType.type_name == type_name)
         ).first()
 
 
@@ -51,22 +43,28 @@ class ContentService:
         self.derived_content_type_repository = DerivedContentTypeRepository(session)
 
     def create_blank_document(
-            self,
-            organization_id: str,
-            workspace_id: str,
-            codebase_id: str,
-            document_name: str | None = None
+        self,
+        organization_id: str,
+        workspace_id: str,
+        codebase_id: str,
+        document_name: str | None = None,
     ) -> DerivedContent:
-        workspace_exists = self.workspace_repository.exists(workspace_id, organization_id)
+        workspace_exists = self.workspace_repository.exists(
+            workspace_id, organization_id
+        )
 
         if not workspace_exists:
             raise NoResultFound("Workspace not found")
 
         # get application note derived content type
-        application_note_content_type = self.derived_content_type_repository.get_by_type_name("application_note")
+        application_note_content_type = (
+            self.derived_content_type_repository.get_by_type_name("application_note")
+        )
 
         # get codebase derived content type
-        codebase_content_type = self.derived_content_type_repository.get_by_type_name("codebase")
+        codebase_content_type = self.derived_content_type_repository.get_by_type_name(
+            "codebase"
+        )
 
         # find the derived content type with content type codebase and workspace id and codebase id
         parent_content = self.session.exec(
@@ -79,27 +77,31 @@ class ContentService:
         blank_content_template = {
             "name": "Untitled" if document_name is None else document_name,
             "content": " ",
-            "description": ""
+            "description": "",
         }
 
-        new_content = self.content_repository.create(DerivedContent(
-            content_type_id=application_note_content_type.id,
-            workspace_id=workspace_id,
-            source_content_id=parent_content.id,
-            codebase_id=codebase_id,
-            relative_path=parent_content.relative_path,
-            content=json.dumps(blank_content_template),
-            misc_metadata={},
-            status=Enum_Derived_Content_Status.generation_complete,
-            created_at=datetime.now(),
-            updated_at=datetime.now(),
-        ))
+        new_content = self.content_repository.create(
+            DerivedContent(
+                content_type_id=application_note_content_type.id,
+                workspace_id=workspace_id,
+                source_content_id=parent_content.id,
+                codebase_id=codebase_id,
+                relative_path=parent_content.relative_path,
+                content=json.dumps(blank_content_template),
+                misc_metadata={},
+                status=Enum_Derived_Content_Status.generation_complete,
+                created_at=datetime.now(),
+                updated_at=datetime.now(),
+            )
+        )
 
         return new_content
 
     def create_document_from_template(self, content_id: str) -> DerivedContent:
         # get the template content type
-        template_content_type = self.derived_content_type_repository.get_by_type_name("template")
+        template_content_type = self.derived_content_type_repository.get_by_type_name(
+            "template"
+        )
         # get the content
         content = self.session.exec(
             select(DerivedContent)
@@ -118,25 +120,30 @@ class ContentService:
         new_content_template["name"] = f"{new_content_name} (Copy)"
         new_content_content = json.dumps(new_content_template)
 
-        application_note_content_type = self.derived_content_type_repository.get_by_type_name("application_note")
+        application_note_content_type = (
+            self.derived_content_type_repository.get_by_type_name("application_note")
+        )
 
         # create a new content from the template
-        new_content = self.content_repository.create(DerivedContent(
-            content_type_id=application_note_content_type.id,
-            workspace_id=content.workspace_id,
-            source_content_id=content.source_content_id,
-            codebase_id=content.codebase_id,
-            relative_path=content.relative_path,
-            content=new_content_content,
-            misc_metadata={},
-            status=Enum_Derived_Content_Status.generation_complete,
-            created_at=datetime.now(),
-            updated_at=datetime.now(),
-        ))
+        new_content = self.content_repository.create(
+            DerivedContent(
+                content_type_id=application_note_content_type.id,
+                workspace_id=content.workspace_id,
+                source_content_id=content.source_content_id,
+                codebase_id=content.codebase_id,
+                relative_path=content.relative_path,
+                content=new_content_content,
+                misc_metadata={},
+                status=Enum_Derived_Content_Status.generation_complete,
+                created_at=datetime.now(),
+                updated_at=datetime.now(),
+            )
+        )
         return new_content
 
-    def get_list_content(self, organization_id: str, search_input: ListContentInput) -> ListContentResults:
-
+    def get_list_content(
+        self, organization_id: str, search_input: ListContentInput
+    ) -> ListContentResults:
         try:
             results, total_count = self._get_list_content(organization_id, search_input)
         except ValueError as e:
@@ -151,12 +158,12 @@ class ContentService:
                 content_name=(
                     json.loads(result.content).get("name")
                     if result.content_type.type_name == "application_note"
-                       and result.content
-                       and "name" in json.loads(result.content)
+                    and result.content
+                    and "name" in json.loads(result.content)
                     else "Generating content..."
                     if result.content_type.type_name == "application_note"
-                       and result.content
-                       and "name" not in json.loads(result.content)
+                    and result.content
+                    and "name" not in json.loads(result.content)
                     else result.relative_path.removeprefix("documents/")
                     if result.content_type.type_name == "pdf_summary"
                     else result.relative_path
@@ -185,9 +192,7 @@ class ContentService:
         )
 
     def _get_list_content(
-            self,
-            organization_id: str,
-            search_input: ListContentInput
+        self, organization_id: str, search_input: ListContentInput
     ) -> tuple[list[DerivedContent], int]:
         statement = (
             select(DerivedContent)
@@ -213,25 +218,35 @@ class ContentService:
         )
         if search_input.sort_by:
             if not hasattr(self.content_repository.model, search_input.sort_by):
-                raise ValueError(f"Invalid sort field '{search_input.sort_by}' for model '{self.content_repository.model.__tablename__}'.")
+                raise ValueError(
+                    f"Invalid sort field '{search_input.sort_by}' for model '{self.content_repository.model.__tablename__}'."
+                )
 
-            field_name = f"{self.content_repository.__tablename__}.{search_input.sort_by}"
+            field_name = (
+                f"{self.content_repository.model.__tablename__}.{search_input.sort_by}"
+            )
             if search_input.sort_direction == "ASC":
                 statement = statement.order_by(asc(text(field_name)))
             elif search_input.sort_direction == "DESC":
                 statement = statement.order_by(desc(text(field_name)))
             else:
-                raise ValueError("Invalid sort direction provided. Options are ASC or DESC")
+                raise ValueError(
+                    "Invalid sort direction provided. Options are ASC or DESC"
+                )
 
         if search_input.text:
-            statement = statement.where(DerivedContent.relative_path.contains(search_input.text))
+            statement = statement.where(
+                DerivedContent.relative_path.contains(search_input.text)
+            )
             count_statement = count_statement.where(
                 DerivedContent.relative_path.contains(search_input.text)
             )
 
         if search_input.status:
             statement = statement.where(DerivedContent.status == search_input.status)
-            count_statement = count_statement.where(DerivedContent.status == search_input.status)
+            count_statement = count_statement.where(
+                DerivedContent.status == search_input.status
+            )
 
         if search_input.content_type_id:
             statement = statement.where(
@@ -264,33 +279,45 @@ class ContentService:
             count_statement = count_statement.where(or_(*tag_id_clauses))
 
         total_count = self.session.exec(count_statement).one()
-        results = self.session.exec(statement.offset(search_input.offset).limit(search_input.limit)).all()
+        results = self.session.exec(
+            statement.offset(search_input.offset).limit(search_input.limit)
+        ).all()
         return results, total_count
 
-    def get_list_content_types(self, lct_inputs: ListContentTypesInput) -> ListContentTypesResults:
+    def get_list_content_types(
+        self, lct_inputs: ListContentTypesInput
+    ) -> ListContentTypesResults:
         try:
             results = self.derived_content_type_repository.get_all(
                 lct_inputs.limit,
                 lct_inputs.offset,
                 lct_inputs.sort_by,
-                lct_inputs.sort_direction
+                lct_inputs.sort_direction,
             )
         except ValueError as e:
             raise HTTPException(status_code=400, detail=str(e))
 
         return ListContentTypesResults(results=results)
 
-    def create_template(self, organization_id: str, workspace_id: str, codebase_id: str) -> DerivedContent:
-        workspace_exists = self.workspace_repository.exists(workspace_id, organization_id)
+    def create_template(
+        self, organization_id: str, workspace_id: str, codebase_id: str
+    ) -> DerivedContent:
+        workspace_exists = self.workspace_repository.exists(
+            workspace_id, organization_id
+        )
 
         if not workspace_exists:
             raise NoResultFound("Workspace not found")
 
         # get application note derived content type
-        template_content_type = self.derived_content_type_repository.get_by_type_name("template")
+        template_content_type = self.derived_content_type_repository.get_by_type_name(
+            "template"
+        )
 
         # get codebase derived content type
-        codebase_content_type = self.derived_content_type_repository.get_by_type_name("codebase")
+        codebase_content_type = self.derived_content_type_repository.get_by_type_name(
+            "codebase"
+        )
 
         # find the derived content type with content type codebase and workspace id and codebase id
         parent_content = self.session.exec(
@@ -300,24 +327,22 @@ class ContentService:
             .where(DerivedContent.codebase_id == codebase_id)
         ).first()
 
-        blank_content_template = {
-            "name": "Template",
-            "content": " ",
-            "description": ""
-        }
+        blank_content_template = {"name": "Template", "content": " ", "description": ""}
         # Add the new content to the session and commit
-        new_content = self.content_repository.create(DerivedContent(
-            content_type_id=template_content_type.id,
-            workspace_id=workspace_id,
-            source_content_id=parent_content.id,
-            codebase_id=codebase_id,
-            relative_path=parent_content.relative_path,
-            content=json.dumps(blank_content_template),
-            misc_metadata={},
-            status=Enum_Derived_Content_Status.generation_complete,
-            created_at=datetime.now(),
-            updated_at=datetime.now(),
-        ))
+        new_content = self.content_repository.create(
+            DerivedContent(
+                content_type_id=template_content_type.id,
+                workspace_id=workspace_id,
+                source_content_id=parent_content.id,
+                codebase_id=codebase_id,
+                relative_path=parent_content.relative_path,
+                content=json.dumps(blank_content_template),
+                misc_metadata={},
+                status=Enum_Derived_Content_Status.generation_complete,
+                created_at=datetime.now(),
+                updated_at=datetime.now(),
+            )
+        )
 
         return new_content
 

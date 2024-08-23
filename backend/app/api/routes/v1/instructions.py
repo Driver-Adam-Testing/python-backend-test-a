eaaa5a90eaa4c4e6743086d26fbb75a3feef8393
Eric Miller <146128069+eric-driverai@users.noreply.github.com>
2024-08-23 05:41:49 -0500

    restoring develop version of instructions

--- backend/app/api/routes/v1/instructions.py
+++ backend/app/api/routes/v1/instructions.py
@@ -1,6 +1,6 @@
 import logging
 
-from database.models_v1 import Workspace
+from database.models_v1 import Workspace, Codebase
 from fastapi import APIRouter, HTTPException
 from sqlmodel import select
 
@@ -18,78 +18,32 @@ router = APIRouter()
 logger = logging.getLogger(__name__)
 
 
-
-@router.post("/", response_model=ExecuteInstructionResponse, deprecated=True)
+@router.post("/execute", response_model=ExecuteInstructionResponse)
 def execute_instruction(
-    session: CurrentSession, body: ExecuteInstructionRequest, user: CurrentUser
+        session: CurrentSession, body: ExecuteInstructionRequest, user: CurrentUser
 ) -> ExecuteInstructionResponse:
-    requested_org = session.exec(
-        select(Workspace).where(Workspace.id == body.workspace_id)
+    result = session.exec(
+        select(Workspace, Codebase)
+        .where(Workspace.id == body.workspace_id)
+        .where(Workspace.organization_id == user.organization_id)
+        .where(Codebase.id == body.codebase_id)
+        .where(Codebase.workspace_id == body.workspace_id)
     ).first()
-    if requested_org is None:
-        logger.error(f"Workspace {body.workspace_id} not found")
-        raise HTTPException(
-            status_code=404,
-            detail="Workspace not found",
-        )
-    if user.organization_id == requested_org.organization_id:
-        call_id = modal_interface.execute_instruction(
-            body.workspace_id, body.codebase_id, body.prompt
-        )
-        return ExecuteInstructionResponse(
-            call_id=call_id,
-        )
-    logger.error(
-        f"User {user.user_id} requested execution outside of their organization."
-    )
-    raise HTTPException(
-        status_code=403,
-        detail="Invalid organization provided",
-    )
 
-@router.post("/execute", response_model=ExecuteInstructionResponse, deprecated=True)
-def execute_instruction(
-    session: CurrentSession, body: ExecuteInstructionRequest, user: CurrentUser
-) -> ExecuteInstructionResponse:
-    requested_org = session.exec(
-        select(Workspace).where(Workspace.id == body.workspace_id)
-    ).first()
-    if requested_org is None:
-        logger.error(f"Workspace {body.workspace_id} not found")
-        raise HTTPException(
-            status_code=404,
-            detail="Workspace not found",
-        )
-    if user.organization_id == requested_org.organization_id:
-        call_id = modal_interface.execute_instruction(
-            body.workspace_id, body.codebase_id, body.prompt
-        )
-        return ExecuteInstructionResponse(
-            call_id=call_id,
-        )
-    logger.error(
-        f"User {user.user_id} requested execution outside of their organization."
-    )
-    raise HTTPException(
-        status_code=403,
-        detail="Invalid organization provided",
-    )
+    if not result:
+        logger.error(
+            f"Invalid workspace or codebase for workspace_id {body.workspace_id} and codebase_id {body.codebase_id}")
+        raise HTTPException(status_code=400, detail="Invalid workspace or codebase")
 
-#TODO deprecated
-@router.post("/results", response_model=InstructionResultsResponse, deprecated=True)
-def get_execution_results(body: BatchInstructionResultsRequest) -> InstructionResultsResponse:
+    call_id = modal_interface.execute_instruction(
+        str(body.workspace_id), str(body.codebase_id), body.prompt
+    )
+    return ExecuteInstructionResponse(call_id=call_id)
+
+
+@router.post("/results", response_model=InstructionResultsResponse)
+def get_execution_results(
+        body: BatchInstructionResultsRequest, user: CurrentUser
+) -> InstructionResultsResponse:
     batch_results = modal_interface.get_batch_execution_results(body.call_ids)
-    return InstructionResultsResponse(
-        data=batch_results
-    )
-
-
-@router.post("/results/search", response_model=InstructionResultsResponse)
-def get_execution_search_results(body: BatchInstructionResultsRequest) -> InstructionResultsResponse:
-    batch_results = modal_interface.get_batch_execution_results(body.call_ids)
-    return InstructionResultsResponse(
-        data=batch_results
-    )
-
-
-
+    return InstructionResultsResponse(data=batch_results)

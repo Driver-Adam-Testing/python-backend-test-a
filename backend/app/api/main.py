052b7af46c3eac7012a5409bae3b390c13200efb
Eric Miller <146128069+eric-driverai@users.noreply.github.com>
2024-09-10 11:02:37 -0500

    Remove internal API route handling
    Deleted the `internal.py` file, removing the handling of internal content listing API route. Also updated `main.py` to exclude the internal router from the API setup.

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -6,7 +6,6 @@ from app.api.routes.v1 import (
     git_provider,
     healthcheck,
     instructions,
-    internal,
     onboarding,
     search,
     tags,
@@ -30,7 +29,6 @@ api_router.include_router(tags.router, prefix="/tags", tags=["tags"])
 api_router.include_router(
     instructions.router, prefix="/instructions", tags=["instructions"]
 )
-api_router.include_router(internal.router, prefix="/internal", tags=["internal"])
 api_router.include_router(upload.router, prefix="/upload", tags=["upload"])
 
 if settings.ENVIRONMENT != "production":

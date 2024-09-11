f43b20734bac060ab78844c98648a1d9e78cd0d9
Jesse Goeglein <jesse.goeglein@driverai.com>
2024-09-11 11:08:30 -0600

    Make the PDF onboarding lambda an S3 admin to create org buckets

--- cdk/constructs/code_onboarding_lambda.py
+++ cdk/constructs/code_onboarding_lambda.py
@@ -1,5 +1,6 @@
 from aws_cdk import (
     Duration,
+    aws_iam,
     aws_lambda,
     aws_lambda_event_sources,
     aws_lambda_python_alpha,
@@ -66,4 +67,12 @@ class CodeOnboardingLambda(Construct):
             aws_s3.EventType.OBJECT_CREATED,
             aws_s3_notifications.SnsDestination(sns_topic),
             aws_s3.NotificationKeyFilter(prefix="codebases/"),
+        )
+
+        # TODO: We should find a way to scope down these privileges.
+        # Because we need to create arbitrary buckets per org,
+        # it's not clear how to do so without breaking existing
+        # functionality.
+        lambda_function.role.add_managed_policy(
+            aws_iam.ManagedPolicy.from_aws_managed_policy_name("AmazonS3FullAccess")
         )

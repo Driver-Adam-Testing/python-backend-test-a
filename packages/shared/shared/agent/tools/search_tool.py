8a51c388db9603f5db369d41d03bf349de6bd75c
Neil Zumwalde <neilzumwalde@gmail.com>
2024-09-26 15:16:34 -0700

    search tool todo

--- packages/shared/shared/agent/tools/search_tool.py
+++ packages/shared/shared/agent/tools/search_tool.py
@@ -126,7 +126,7 @@ class SearchTool(ToolStrict):
             metadata = result.metadata
             content_type = metadata.get("content_type", "")
             relative_path = metadata.get("relative_path", "")
-
+            # TODO: add formatting to the interface. This would allow us to share then
             formatted_result = f"""<result>
                 <content>{content}</content>
                 <content_type>{content_type}</content_type>

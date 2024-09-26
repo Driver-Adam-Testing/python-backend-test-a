9d9d0b7d0e5fdd15dde041d9bef3e88d6e89304d
Neil Zumwalde <neilzumwalde@gmail.com>
2024-09-26 15:14:44 -0700

    improved copy editor language

--- packages/shared/shared/prompts/voice/copy_editor_remove_useless_language.py
+++ packages/shared/shared/prompts/voice/copy_editor_remove_useless_language.py
@@ -1,5 +1,8 @@
-PROMPT = """Remove congratulatory language from the text.
+PROMPT = """
+Remove congratulatory language from the text.
 Remove language from the text that uses words that describes and congratulates the social qualities of a technical product. Sentences that qualify often include words that in the language families of "pivotal", "essential", "critical", "robust", "integral". "rigorous", "comprehensive", and "valuable", "meticulously", "vital", and "exemplified".
+Remove text that is extremely generic.
+Remove text that is redundant.
 """
 
 MESSAGE = {"role": "system", "content": PROMPT}

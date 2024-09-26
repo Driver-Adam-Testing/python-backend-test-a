bf9a9c9315b9e86971d5611f40b39fc4279ac4b4
Daniel Hensley <daniel.hensley@driverai.com>
2024-09-26 15:18:14 -0500

    feat: render C and C++ with new schema

--- content_services/inspector/src/inspection/prompt_templates/files/templates/source_code_large_cpp.py
+++ content_services/inspector/src/inspection/prompt_templates/files/templates/source_code_large_cpp.py
@@ -1,9 +1,6 @@
 from utils.lang_specialization.cpp import (
-    DATA_STRUCTURES_NONE_CONTENT,
-    FUNCTIONS_NONE_CONTENT,
     SOURCE_CODE_LARGE_PURPOSE_USER_PROMPT,
     SOURCE_CODE_LARGE_SYSTEM_PROMPT_GENERAL_CPP,
-    VARIABLES_NONE_CONTENT,
     class_dict_from_llm_cpp,
     cpp_class_checker,
     cpp_data_structure_checker,
@@ -14,7 +11,6 @@ from utils.lang_specialization.cpp import (
     variables_dict_from_llm_cpp,
 )
 from utils.lang_specialization.default import (
-    IMPORTS_NONE_CONTENT,
     default_imports_checker,
 )
 from utils.templates import S
@@ -26,40 +22,39 @@ SOURCE_CODE_LARGE_TEMPLATE_CPP = [
         SOURCE_CODE_LARGE_SYSTEM_PROMPT_GENERAL_CPP,
         SOURCE_CODE_LARGE_PURPOSE_USER_PROMPT,
     ),
-    (S.RAW, "# Symbol Documentation"),
     (
         S.LLM_COND_JSON,
-        "\n---\n## Imports and Dependencies",
+        "# Imports and Dependencies",
         default_imports_checker,
         lambda _llm, output, _code: output,
-        IMPORTS_NONE_CONTENT,
+        None,
     ),
     (
         S.FN_COND_JSON,
-        "\n---\n## Global Variables",
+        "# Global Variables",
         cpp_variables_checker,
         variables_dict_from_llm_cpp,
-        VARIABLES_NONE_CONTENT,
+        None,
     ),
     (
         S.FN_COND_JSON,
-        "\n---\n## Classes and Structs",
+        "# Classes and Structs",
         cpp_class_checker,
         class_dict_from_llm_cpp,
-        DATA_STRUCTURES_NONE_CONTENT,
+        None,
     ),
     (
         S.FN_COND_JSON,
-        "\n---\n## Free Functions",
+        "# Free Functions",
         cpp_function_checker,
         fn_dict_from_llm_cpp,
-        FUNCTIONS_NONE_CONTENT,
+        None,
     ),
     (
         S.FN_COND_JSON,
-        "\n---\n## Data Structures",
+        "# Data Structures",
         cpp_data_structure_checker,
         data_structure_dict_from_llm_cpp,
-        DATA_STRUCTURES_NONE_CONTENT,
+        None,
     ),
 ]

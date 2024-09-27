a9431457f3fc1aa58d345d7bc8122db7a14c7450
Neil Zumwalde <neilzumwalde@gmail.com>
2024-09-27 13:37:18 -0700

    streamline copy editor system prompts

--- packages/shared/shared/prompts/voice/__init__.py
+++ packages/shared/shared/prompts/voice/__init__.py
@@ -1,8 +1,5 @@
 # flake8: noqa
 from . import (
     copy_editor,
-    copy_editor_remove_conclusions,
-    copy_editor_remove_speculation,
-    copy_editor_remove_useless_language,
     software_engineer,
 )
--- packages/shared/shared/prompts/voice/copy_editor_remove_speculation.py
+++
@@ -1,5 +0,0 @@
-PROMPT = """
-Remove all speculation.
-If a text is speculative in it's entirety, remove it and admit you didn't have enough context to create non-speculative content. """
-
-MESSAGE = {"role": "system", "content": PROMPT}

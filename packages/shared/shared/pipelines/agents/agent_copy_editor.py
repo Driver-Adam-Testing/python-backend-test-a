0c850fba349745a7b003a0211700879a40922293
Neil Zumwalde <neilzumwalde@gmail.com>
2024-11-04 10:54:48 -0800

    add .MESSAGE to prompt

--- packages/shared/shared/pipelines/agents/agent_copy_editor.py
+++ packages/shared/shared/pipelines/agents/agent_copy_editor.py
@@ -16,7 +16,7 @@ def run_agent_copy_editor(input: PipelineStepConfiguration) -> PipelineStepRespo
     for system_prompt in input.create_system_prompts():
         agent.add_message(system_prompt)
     if "```mermaid" in str(input.prompt):
-        agent.add_message(prompts.task.codeblock_syntax_mermaid)
+        agent.add_message(prompts.task.codeblock_syntax_mermaid.MESSAGE)
     agent.add_message(prompts.voice.copy_editor.MESSAGE)
     response = agent.invoke(str(input.prompt))
 

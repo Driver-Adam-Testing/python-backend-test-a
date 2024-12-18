40129965918f4d7892b5e82d6306b0c4b9f1ecd3
Andrew Mark <andrewemark@gmail.com>
2024-12-18 13:34:23 -0800

    Improved logging

--- backend/app/utils/gh_ops.py
+++ backend/app/utils/gh_ops.py
@@ -175,7 +175,8 @@ def fetch_default_branch_and_commit(org_name: str, repo: str, access_token: str)
 
     branch_url = f"{repo_url}/branches/{default_branch}"
     branch_data = requests.get(branch_url, headers=headers).json()
-
+    logger.info(f"Default branch for {repo} in {org_name} is {default_branch}")
+    logger.info(f"Branch data from github API: {branch_data}")
     return branch_data["commit"]["sha"]
 
 
@@ -278,9 +279,7 @@ def download_and_upload_repo(
             logger.info("Repository %s uploaded successfully to %s.", repo, upload_key)
 
         return success, analysis_download_url
-    except requests.RequestException as e:
-        logger.error("Request error: %s", e)
-    except Exception as e:
-        logger.error("Unexpected error: %s", e)
+    except Exception:
+        logger.exception("Unexpected error downloading/uploading repo.")
 
     return False, ""

8c57128a23c08ab7f41ee843cde018b849d37662
Neil Zumwalde <neilzumwalde@gmail.com>
2025-01-06 15:03:51 -0800

    migrate assettags

--- driver_db/database/nodes_v2_sql/migrate_tags.py
+++ driver_db/database/nodes_v2_sql/migrate_tags.py
@@ -1,29 +1,23 @@
 MIGRATE_TAGS = """
 
 WITH tags_primary_asset_ids AS (
-    SELECT
-        t.tag_id as tag_id,
-        t.content_id as content_id
-        CASE
-            WHEN pa.id IS NOT NULL
-                THEN pa.id -- this should cover codebases, pages, and templates
-            WHEN pa.id IS NULL AND v.id IS NOT NULL
-                THEN v.primary_asset_id -- this should cover pdfs
-        END as primary_asset_id
-    FROM tags_contents t
-    LEFT JOIN v2_primary_asset pa on t.content_id = pa.id
-    LEFT JOIN v2_version v on t.content_id = v.id
-),
-ins_v2_primary_asset_tag AS (
-    INSERT INTO v2_primary_asset_tag (
-        tag_id,
-        primary_asset_id
-    )
-    SELECT
-        tag_id,
-        primary_asset_id
-    FROM tags_primary_asset_ids
-    RETURNING tag_id
+SELECT DISTINCT
+    t.tag_id as tag_id,
+    v.primary_asset_id as primary_asset_id
+FROM tags_contents t
+JOIN derived_contents dc on t.content_id = dc.id
+LEFT JOIN v2_node n on (dc.node_id = n.id or n.relative_path = CONCAT(dc.relative_path, '/'))
+LEFT JOIN v2_version v on v.id = n.version_id
+where v.primary_asset_id is NOT NULL
 )
+INSERT INTO v2_primary_asset_tag (
+    tag_id,
+    primary_asset_id
+)
+SELECT
+    tag_id,
+    primary_asset_id
+FROM tags_primary_asset_ids
+RETURNING tag_id;
 
 """

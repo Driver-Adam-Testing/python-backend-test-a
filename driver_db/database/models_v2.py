16c0ab846f4f2e3daa7f597fbe9affa989509bdf
Neil Zumwalde <neilzumwalde@gmail.com>
2025-01-13 13:51:09 -0800

    init

--- driver_db/database/models_v2.py
+++ driver_db/database/models_v2.py
@@ -1,3 +1,4 @@
+import hashlib
 import uuid
 from datetime import datetime
 from typing import Optional
@@ -159,6 +160,13 @@ class Node(SQLModel, table=True):  # type: ignore
         back_populates="page_node",
         sa_relationship_kwargs={"foreign_keys": "DocumentSource.page_node_id"},
     )
+
+    @property
+    def s3_url(self) -> str:
+        org_id_hash = hashlib.sha256(self.version.organization_id.encode()).hexdigest()[
+            :63
+        ]
+        return f"https://{org_id_hash}.s3.amazonaws.com/{self.version.primary_asset_id}/{self.version_id}/{self.relative_path}"
 
 
 class PrimaryAssetTag(SQLModel, table=True):

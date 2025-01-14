f3192f3a6ea76620bc5612eccff0a5fa246dc4dc
Neil Zumwalde <neilzumwalde@gmail.com>
2025-01-14 10:16:34 -0800

    init

--- driver_db/database/models_v2.py
+++ driver_db/database/models_v2.py
@@ -46,7 +46,13 @@ class PrimaryAsset(SQLModel, table=True):  # type: ignore
         ),
         default=None,
     )
-    versions: list["Version"] = Relationship(back_populates="primary_asset")
+    versions: list["Version"] = Relationship(
+        back_populates="primary_asset",
+        sa_relationship_kwargs={
+            "passive_deletes": True,
+            "cascade": "all, delete-orphan",
+        },
+    )
     tags: list["Tag"] = Relationship(  # noqa: F821
         back_populates="primary_assets",
         sa_relationship_kwargs={"secondary": "v2_primary_asset_tag"},
@@ -94,7 +100,13 @@ class Version(SQLModel, table=True):  # type: ignore
         default=None,
     )
     primary_asset: "PrimaryAsset" = Relationship(back_populates="versions")
-    nodes: list["Node"] = Relationship(back_populates="version")
+    nodes: list["Node"] = Relationship(
+        back_populates="version",
+        sa_relationship_kwargs={
+            "cascade": "all, delete-orphan",
+            "passive_deletes": True,
+        },
+    )
     root_node: Optional["Node"] = Relationship(
         sa_relationship_kwargs={
             "primaryjoin": "and_(Version.id == Node.version_id)",
@@ -140,7 +152,13 @@ class Node(SQLModel, table=True):  # type: ignore
         default=None,
     )
     version: "Version" = Relationship(back_populates="nodes")
-    contents: list["DerivedContent"] = Relationship(back_populates="node")  # noqa: F821
+    contents: list["DerivedContent"] = Relationship(  # noqa: F821
+        back_populates="node",
+        sa_relationship_kwargs={
+            "cascade": "all, delete-orphan",
+            "passive_deletes": True,
+        },
+    )
 
     parent_node: Optional["Node"] = Relationship(
         sa_relationship_kwargs={
@@ -171,5 +189,7 @@ class Node(SQLModel, table=True):  # type: ignore
 
 class PrimaryAssetTag(SQLModel, table=True):
     __tablename__ = "v2_primary_asset_tag"
-    tag_id: UUID = Field(primary_key=True, foreign_key="tags.id")
-    primary_asset_id: UUID = Field(primary_key=True, foreign_key="v2_primary_asset.id")
+    tag_id: UUID = Field(primary_key=True, ondelete="CASCADE", foreign_key="tags.id")
+    primary_asset_id: UUID = Field(
+        primary_key=True, ondelete="CASCADE", foreign_key="v2_primary_asset.id"
+    )

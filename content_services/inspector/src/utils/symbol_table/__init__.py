1aedbd31c59173b0284b619477f45f90389b506d
Shane  Ghiotto <sghiotto1@gmail.com>
2025-06-04 20:40:11 -0700

    Remove detect_languages from __init__.py

--- content_services/inspector/src/utils/symbol_table/__init__.py
+++ content_services/inspector/src/utils/symbol_table/__init__.py
@@ -1,8 +1,7 @@
-from .language_utils import detect_language, get_supported_languages
+from .language_utils import get_supported_languages
 from .orchestrator import build_symbol_table, print_summary
 
 __all__ = [
-    "detect_language",
     "get_supported_languages",
     "build_symbol_table",
     "print_summary",

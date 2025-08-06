ad71cdc75e68423f7d4d5b084f8c7fb3ea0012f1
Eric Miller <146128069+eric-driverai@users.noreply.github.com>
2025-08-06 12:07:00 -0500

    fix: deploy hack

--- cdk/pms_stack.py
+++ cdk/pms_stack.py
@@ -27,31 +27,31 @@ class PMSStack(Stack):
                 cloudwatch_alarm_arn="arn:aws:sns:us-east-1:537622164442:CloudwatchAlarms",
             ),
         )
-        # self.backend = Backend(
-        #     self,
-        #     "ApiBackend",
-        #     BackendParams(
-        #         environment="pms",
-        #         cors_origins=[cors_origins],
-        #         allowed_ips=[],  # All IPs currently allowed
-        #         use_legacy_dropzone=True,
-        #         metrics_bus=self.metrics_lambda.metrics_bus,
-        #         aws_region=self.cdkenv.region,
-        #         aws_account=self.cdkenv.account
-        #     ),
-        # )
-        # self.onboarding_lambda = AssetOnboardingLambda(
-        #     self,
-        #     "AssetOnboardingLambda",
-        #     AssetOnboardingLambdaParams(
-        #         environment="pms",
-        #         api_url="https://api.pms.driverai.com/studio/v1",
-        #         auth0_url="https://auth.pms.driverai.com",
-        #         auth0_audience="https://api.pms.driverai.com/api/v1",
-        #         dropzone_bucket=self.backend.dropzone_bucket,
-        #         use_legacy_dropzone=True,
-        #     ),
-        # )
+        self.backend = Backend(
+            self,
+            "ApiBackend",
+            BackendParams(
+                environment="pms",
+                cors_origins=[cors_origins],
+                allowed_ips=[],  # All IPs currently allowed
+                use_legacy_dropzone=True,
+                metrics_bus=self.metrics_lambda.metrics_bus,
+                aws_region=self.cdkenv.region,
+                aws_account=self.cdkenv.account
+            ),
+        )
+        self.onboarding_lambda = AssetOnboardingLambda(
+            self,
+            "AssetOnboardingLambda",
+            AssetOnboardingLambdaParams(
+                environment="pms",
+                api_url="https://api.pms.driverai.com/studio/v1",
+                auth0_url="https://auth.pms.driverai.com",
+                auth0_audience="https://api.pms.driverai.com/api/v1",
+                dropzone_bucket=self.backend.dropzone_bucket,
+                use_legacy_dropzone=True,
+            ),
+        )
         self.inspector = Inspector(
             self, "Inspector", InspectorParams(environment="pms")
         )

69a37c561badbb3becc9f15109865e8ac1b782af
Andrew Mark <andrewemark@gmail.com>
2025-09-03 11:23:48 -0700

    Refactor import statements in vcs_auto_update.py to use updated database models

--- backend/app/git_providers/utils/vcs_auto_update.py
+++ backend/app/git_providers/utils/vcs_auto_update.py
@@ -1,8 +1,7 @@
 from datetime import UTC, datetime
 from uuid import UUID
 
-from database.models_v2 import PrimaryAsset, Version
-from database.models_v2_enums import PrimaryAssetKind, VcsAutoUpdatePolicy
+from database.models import PrimaryAsset, PrimaryAssetKind, VcsAutoUpdatePolicy, Version
 from sqlmodel import Session, select
 
 

a3457a64cbcd191678ef2becaf9230199bdace74
Nick Wellinghoff <niwellin@silabs.com>
2025-10-01 11:07:26 -0500

    set static bucket name

--- cdk/constructs/backend.py
+++ cdk/constructs/backend.py
@@ -78,7 +78,7 @@ class Backend(Construct):
         self.dropzone_bucket = aws_s3.Bucket(
             self,
             "DropzoneBucket",
-            bucket_name=f"{params.environment}-codebase-dropzone"
+            bucket_name=f"{settings.DEPLOYMENT_ENVIRONMENT}-codebase-dropzone",
             cors=[
                 {
                     "allowedMethods": [

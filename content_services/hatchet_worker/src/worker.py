155a0b3970f8a2e914fcb188a32d124962be2ba4
Shane  Ghiotto <sghiotto1@gmail.com>
2025-10-09 09:19:02 -0700

    Working PDF processing example

--- content_services/hatchet_worker/src/worker.py
+++ content_services/hatchet_worker/src/worker.py
@@ -1,9 +1,12 @@
 from hatchet_client import hatchet
 from workflows.first_workflow import my_task
+from workflows.pdf_processing_workflow import pdf_processing_task
 
 
 def main() -> None:
-    worker = hatchet.worker("test-worker", slots=1, workflows=[my_task])
+    worker = hatchet.worker(
+        "test-worker", slots=1, workflows=[my_task, pdf_processing_task]
+    )
     worker.start()
 
 

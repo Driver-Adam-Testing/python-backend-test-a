6ab75039adf57925499966c3cca61c71dae1e2ed
Eric Miller <146128069+eric-driverai@users.noreply.github.com>
2025-10-14 23:00:23 -0500

    docs: add clarification comment about Auth0EventStack purpose for testing

--- cdk/auth0_event_stack.py
+++ cdk/auth0_event_stack.py
@@ -5,6 +5,11 @@ from constructs import Construct
 
 from cdk.constructs.auth0_event_lambda import Auth0EventLambda, Auth0EventLambdaParams
 
+"""
+This stack is not required!!!
+It is only here to allow deploying the Auth0EventLambda construct in isolation for testing purposes.
+"""
+
 
 class Auth0EventStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs: any) -> None:

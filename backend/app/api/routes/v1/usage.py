be1871a7acc4a52b2bb75dcf6740e94e28ea51bf
Jesse Goeglein <jesse.goeglein@driverai.com>
2025-11-03 08:59:26 -0700

    Disable broken and unsecured usage credit endpoint with notes

--- backend/app/api/routes/v1/usage.py
+++ backend/app/api/routes/v1/usage.py
@@ -70,37 +70,47 @@ def get_charges(
     )
 
 
-@router.post(
-    "/credit",
-    summary="Issue Usage Credits",
-    dependencies=[UsageCreditPermission],
-)
-def credit_usage(
-    session: CurrentSession,
-    current_token: M2MToken,
-    credit_usage_event: CreditUsageEvent,
-) -> JSONResponse:
-    # TODO - fix M2M token validation. This should not be callable by customers. It is currently broken.
-    if current_token is None:
-        raise HTTPException(403, "Forbidden")
+# TODO - fix M2M token validation. This should not be callable by customers. It is currently broken.
+# I think this may start working again when we remove the old auth system. Places where the current impl
+# prevents this from working:
+# 
+# 1. main.py requries user auth (require_jwt) for all endpoints underneath /studio/v1
+# 2. auth.py require_permission requires a user jwt (require_jwt) to check permissions. M2M tokens have permissions, but not a user. They're a machine, not a User.
+# 
+# The require_m2m_jwt in this endpoint never gets reached because of (at least) those reasons as things stand.
+# 
+# jwt_middleware 
+# @router.post(
+#     "/credit",
+#     summary="Issue Usage Credits",
+#     dependencies=[UsageCreditPermission],
+# )
+# def credit_usage(
+#     session: CurrentSession,
+#     current_token: M2MToken,
+#     credit_usage_event: CreditUsageEvent,
+# ) -> JSONResponse:
+   
+#     if current_token is None:
+#         raise HTTPException(403, "Forbidden")
 
-    aws_client = boto3.client(
-        "events",
-        region_name=settings.AWS_REGION,
-        aws_access_key_id=settings.S3ADMIN_AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=settings.S3ADMIN_AWS_SECRET_ACCESS_KEY,
-    )
+#     aws_client = boto3.client(
+#         "events",
+#         region_name=settings.AWS_REGION,
+#         aws_access_key_id=settings.S3ADMIN_AWS_ACCESS_KEY_ID,
+#         aws_secret_access_key=settings.S3ADMIN_AWS_SECRET_ACCESS_KEY,
+#     )
 
-    organization_id = credit_usage_event.organization_id
-    user_id = (
-        "SYSTEM" if credit_usage_event.user_id is None else credit_usage_event.user_id
-    )
-    event_type = UsageEventType.BASE_PLATFORM_USAGE_CREDIT
-    credit_amount = sloc_to_bytes(credit_usage_event.sloc_credit_amount)
+#     organization_id = credit_usage_event.organization_id
+#     user_id = (
+#         "SYSTEM" if credit_usage_event.user_id is None else credit_usage_event.user_id
+#     )
+#     event_type = UsageEventType.BASE_PLATFORM_USAGE_CREDIT
+#     credit_amount = sloc_to_bytes(credit_usage_event.sloc_credit_amount)
 
-    UsageService(session, aws_client).issue_usage_credits(
-        organization_id, user_id, event_type, credit_amount
-    )
-    return JSONResponse(
-        status_code=status.HTTP_202_ACCEPTED, content={"message": "Accepted"}
-    )
+#     UsageService(session, aws_client).issue_usage_credits(
+#         organization_id, user_id, event_type, credit_amount
+#     )
+#     return JSONResponse(
+#         status_code=status.HTTP_202_ACCEPTED, content={"message": "Accepted"}
+#     )

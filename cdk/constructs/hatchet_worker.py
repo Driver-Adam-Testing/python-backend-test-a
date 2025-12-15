de37bd42b42b54b4feb149efc87d5fe0e98d2311
Jesse Goeglein <jesse.goeglein@driverai.com>
2025-12-15 16:56:44 -0700

    Use an integer instead of MB

--- cdk/constructs/hatchet_worker.py
+++ cdk/constructs/hatchet_worker.py
@@ -78,8 +78,8 @@ class HatchetWorker(Construct):
             "ECS_CONTAINER_STOP_TIMEOUT": "2s",
             "HATCHET_CLIENT_HOST_PORT" : f"hatchet.{hosted_zone.zone_name}:7077",
             "INSPECTOR_BUCKET_NAME": inspector_bucket_name,
-            "HATCHET_CLIENT_GRPC_MAX_RECV_MESSAGE_LENGTH": "100MB",
-            "HATCHET_CLIENT_GRPC_MAX_SEND_MESSAGE_LENGTH": "100MB"
+            "HATCHET_CLIENT_GRPC_MAX_RECV_MESSAGE_LENGTH": "100000000",
+            "HATCHET_CLIENT_GRPC_MAX_SEND_MESSAGE_LENGTH": "100000000"
         }
 
         if settings.IS_PRIVATE_DEPLOY == "true":

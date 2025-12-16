f24477127824b3d4a92e3662f609bc6382c52585
Shane  Ghiotto <sghiotto1@gmail.com>
2025-12-16 13:53:15 -0800

    _scale_contents fix

--- content_services/src/autodocs/src/auto_toml/auto_toml.py
+++ content_services/src/autodocs/src/auto_toml/auto_toml.py
@@ -780,7 +780,9 @@ class AutoToml:
             values = [next(iter(content.values())) for content in sub_contents]
 
             for idx, value in enumerate(values):
-                new_value = cls._truncate_text(text=value, scale_factor=scale_factor)
+                new_value = cls._truncate_text(
+                    text=value, llm=None, scale_factor=scale_factor
+                )
                 if new_value is not value:
                     values[idx] = new_value
                     logger.warning(f"Truncated content for: {keys[idx]}\n")

bf826e709700d4028632d70338ecd80d53a237bf
Jesse Goeglein <jesse.goeglein@driverai.com>
2025-12-18 16:33:54 -0700

    Use different DNS

--- cdk/constructs/backend.py
+++ cdk/constructs/backend.py
@@ -133,7 +133,7 @@ class Backend(Construct):
             "AWS_REGION": params.aws_region,
             "ECS_CONTAINER_STOP_TIMEOUT": "2s",
             "IS_PRIVATE_DEPLOY": "true" if params.is_private_deploy else "false",
-            "HATCHET_CLIENT_HOST_PORT": f"hatchet.{hosted_zone.zone_name}:7077",
+            "HATCHET_CLIENT_HOST_PORT": f"hatchet.private.{hosted_zone.zone_name}:7077",
             "HATCHET_CLIENT_TLS_STRATEGY": "none",
             "REDIS_HOST": "hatchet.private." + hosted_zone.zone_name,
             "MCP_STORAGE_BACKEND": "redis",

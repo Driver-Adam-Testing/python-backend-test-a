8ebfa51c8a2eb79040c6bb179650113b831a450f
Nick Wellinghoff <nick.wellinghoff@driverai.com>
2025-12-19 15:31:08 -0600

    added dropzone bucket call back in

--- cdk/backend_stack.py
+++ cdk/backend_stack.py
@@ -40,38 +40,6 @@ class BackendStack(Stack):
             self, "Auth0EventLambda", environment=settings.DEPLOYMENT_ENVIRONMENT
         )
 
-        self.heavyhatchetworker = HatchetWorker(
-            self,
-            "HatchetHeavyWorker",
-            HatchetWorkerParams(
-                environment=settings.DEPLOYMENT_ENVIRONMENT,
-                metrics_bus=self.metrics_lambda.metrics_bus,
-                aws_region=self.cdkenv.region,
-                aws_account=self.cdkenv.account,
-                cpu_size=8192,
-                mem_size=32768,
-                min_instance=1,
-                workflow_set_name=HatchetWorkerType.HEAVY.value,
-                is_private_deploy=settings.IS_PRIVATE_DEPLOY.lower() == "true",
-            ),
-        )
-
-        self.basehatchetworker = HatchetWorker(
-            self,
-            "HatchetBaselineWorker",
-            HatchetWorkerParams(
-                environment=settings.DEPLOYMENT_ENVIRONMENT,
-                metrics_bus=self.metrics_lambda.metrics_bus,
-                aws_region=self.cdkenv.region,
-                aws_account=self.cdkenv.account,
-                cpu_size=2048,
-                mem_size=4096,
-                min_instance=1,
-                workflow_set_name=HatchetWorkerType.BASE.value,
-                is_private_deploy=settings.IS_PRIVATE_DEPLOY.lower() == "true",
-            ),
-        )
-
         self.backend = Backend(
             self,
             "ApiBackend",
@@ -101,14 +69,35 @@ class BackendStack(Stack):
                 is_private_deploy=settings.IS_PRIVATE_DEPLOY.lower() == "true",
             ),
         )
-        self.hatchetworker = HatchetWorker(
+        self.heavyhatchetworker = HatchetWorker(
             self,
-            "HatchetWorker",
+            "HatchetHeavyWorker",
             HatchetWorkerParams(
                 environment=settings.DEPLOYMENT_ENVIRONMENT,
                 metrics_bus=self.metrics_lambda.metrics_bus,
                 aws_region=self.cdkenv.region,
                 aws_account=self.cdkenv.account,
+                cpu_size=8192,
+                mem_size=32768,
+                min_instance=1,
+                workflow_set_name=HatchetWorkerType.HEAVY.value,
+                is_private_deploy=settings.IS_PRIVATE_DEPLOY.lower() == "true",
+                dropzone_bucket=self.backend.dropzone_bucket
+            ),
+        )
+
+        self.basehatchetworker = HatchetWorker(
+            self,
+            "HatchetBaselineWorker",
+            HatchetWorkerParams(
+                environment=settings.DEPLOYMENT_ENVIRONMENT,
+                metrics_bus=self.metrics_lambda.metrics_bus,
+                aws_region=self.cdkenv.region,
+                aws_account=self.cdkenv.account,
+                cpu_size=2048,
+                mem_size=4096,
+                min_instance=1,
+                workflow_set_name=HatchetWorkerType.BASE.value,
                 is_private_deploy=settings.IS_PRIVATE_DEPLOY.lower() == "true",
                 dropzone_bucket=self.backend.dropzone_bucket
             ),

22fe962ab95ae1ec7f36a8780413e2729583f516
Jesse Goeglein <jesse.goeglein@driverai.com>
2025-12-19 09:11:19 -0700

    read_write

--- cdk/constructs/hatchet_worker.py
+++ cdk/constructs/hatchet_worker.py
@@ -206,7 +206,7 @@ class HatchetWorker(Construct):
             aws_iam.ManagedPolicy.from_aws_managed_policy_name("AmazonS3FullAccess")
         )
         # Add explicit perms for dropzone bucket in the event we scope down S3 full access
-        params.dropzone_bucket.grant_write(worker_task_def.task_role)
+        params.dropzone_bucket.grant_read_write(worker_task_def.task_role)
 
         if settings.IS_PRIVATE_DEPLOY == "true":
             firewall_cert_secret = aws_secretsmanager.Secret.from_secret_name_v2(

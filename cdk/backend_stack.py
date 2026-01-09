60859cbe8c7368fe95c0f945d5c63c62cd5631c3
Nick Wellinghoff <nick.wellinghoff@driverai.com>
2026-01-09 12:49:55 -0600

    add analytics worker type to CDK

--- cdk/backend_stack.py
+++ cdk/backend_stack.py
@@ -72,6 +72,23 @@ class BackendStack(Stack):
         )
         self.heavyhatchetworker = HatchetWorker(
             self,
+            "HatchetAnalyticsWorker",
+            HatchetWorkerParams(
+                environment=settings.DEPLOYMENT_ENVIRONMENT,
+                metrics_bus=self.metrics_lambda.metrics_bus,
+                aws_region=self.cdkenv.region,
+                aws_account=self.cdkenv.account,
+                cpu_size=8192,
+                mem_size=32768,
+                min_instance=1,
+                workflow_set_name=HatchetWorkerType.ANALYTICS.value,
+                is_private_deploy=settings.IS_PRIVATE_DEPLOY.lower() == "true",
+                dropzone_bucket=self.backend.dropzone_bucket
+            ),
+        )
+
+        self.heavyhatchetworker = HatchetWorker(
+            self,
             "HatchetHeavyWorker",
             HatchetWorkerParams(
                 environment=settings.DEPLOYMENT_ENVIRONMENT,
